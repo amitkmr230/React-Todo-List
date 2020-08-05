8120075520581f12a038ae620dfbf320a8bcecb3
Amit <you@example.com>
2020-08-05 16:58:20 +0530

    last

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class ListItem extends Component {
     render() {
         return (
-            <li className="list-group-item text-capitalize d-flex justify-content-between my-2 bg-secondary">
+            <li className="list-group-item text-capitalize d-flex justify-content-between my-1 bg-warning">
                 <h4>{this.props.title}</h4>
                 <div>
                     <span className="mx-2 text-success" onClick={this.props.handleEdit}><i className="fa fa-pencil" /></span>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -11,8 +11,8 @@ class TodoList extends Component {
                         <ListItem key={item.id} title={item.item} handleDelete={() => this.props.handleDelete(item.id)} handleEdit={() => this.props.handleEdit(item.id)} />
                     )
                 })}
+                {(this.props.items.length !== 0) ? <button type="button" className="btn btn-danger btn-block mt-5" onClick={this.props.clearList}>Clear List</button> : null}
                 
-                <button type="button" className="btn btn-danger btn-block mt-5" onClick={this.props.clearList}>Clear List</button>
             </ul>
         )
     }
